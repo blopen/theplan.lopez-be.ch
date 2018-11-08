3848e268f4c7cac22475d21c77573ed07972c434
Kenan <kenan.tabinas@iet-gibb.ch>
2018-11-08 13:30:54 +0100

    Add Excersies

--- database_doc/2018-11-08_inital_data.sql
+++ database_doc/2018-11-08_inital_data.sql
@@ -6,3 +6,11 @@ INSERT INTO `user` (`id`, `email`, `firstname`, `lastname`, `password`, `token`,
 (4, 'nelson.lopez1806@gmail.com', 'Nelson', 'Lopez', '0b55630f0d13cd0cad3caa65961da523b9b14e1db47852ec5e45cd0e7181d37a', 'a88e1248b0831c41c733237c3409fc', '2017-07-02 12:26:16', 1, 0, NULL),
 (5, 'nelson.lopez1806@gmail.com', 'Nelson', 'Lopez', '85ea7f74dac1ad47de132462400ffa135a4a64742fea1b61d0823a8efd2614d4', '6cd3c8234fb6dfc9665201cba42f01', '2017-08-10 10:35:17', 1, 0, NULL),
 (10, 'kingkoshtnex@gmail.com', 'Kenan', 'Tabinas', '4fbcf30780984d666b4c3d61c01f4d4e519268dc1b8938af9c4ee586dc99a14d', 'd6270954c0eee77aba0f1300d1dcfd', '2018-10-30 06:41:02', 1, 0, NULL);
+
+INSERT INTO `exercise` (`name`, `user_id`) VALUES ('Langhantel Bankdrücken', '0');
+INSERT INTO `exercise` (`name`, `user_id`) VALUES ('Kurzhantel Bankdrücken', '0');
+INSERT INTO `exercise` (`name`, `user_id`) VALUES ('Kurzhantel Bankdrücken Schreg', '0');
+INSERT INTO `exercise` (`name`, `user_id`) VALUES ('Langhantel Bankdrücken Schreg', '0');
+INSERT INTO `exercise` (`name`, `user_id`) VALUES ('Butterfly Maschine', '0');
+INSERT INTO `exercise` (`name`, `user_id`) VALUES ('Butterfly Freihantel', '0');
+INSERT INTO `exercise` (`name`, `user_id`) VALUES ('Butterfly Kabelzug', '0');
